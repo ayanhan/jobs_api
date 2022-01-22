c951bb52ae643219d303aa4b874026801c9f4ae2
ayanjk <kalkhozhaev.j@gmail.com>
2022-01-22 18:21:16 +0900

    router, mongoose, init

---
+++ controllers/jobs.js
@@ -0,0 +1,23 @@
+const getAllJobs = async (req, res) => {
+  res.send("getAllJobs");
+};
+const getJob = async (req, res) => {
+  res.send("get Job");
+};
+const createJob = async (req, res) => {
+  res.send("createJob");
+};
+const updateJob = async (req, res) => {
+  res.send("updateJob");
+};
+const deleteJob = async (req, res) => {
+  res.send("deleteJob");
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+ };
